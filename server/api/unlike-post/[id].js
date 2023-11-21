96260748862403272677393d9100977d33f3a68b
vetal7777 <jobschildren@gmail.com>
2023-11-21 23:25:01 +0200

    feat: update server routes

--- server/api/unlike-post/[id].js
+++ server/api/unlike-post/[id].js
@@ -1,10 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export default defineEventHandler(async (event) => {
-  const deleted = await prisma.likes.delete({
-    where: { id: Number(event.context.params.id) }
-  })
+export default defineEventHandler(async (event) => {        
+    const deleted = await prisma.likes.delete({
+        where: { id: Number(event.context.params.id) },
+    })
 
-  return deleted
+    return deleted
 })
