07409f2c1362c973f8f4513eb2934061829d764b
vetal7777 <jobschildren@gmail.com>
2023-11-21 23:23:52 +0200

    feat: update Modal

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,39 +1,30 @@
 <template>
-  <div
-    id="Modal"
-    class="fixed bottom-0 z-50 grid h-screen h-screen w-full place-items-center bg-black bg-opacity-50 backdrop-blur-md"
-  >
-    <div
-      class="w-full max-w-[300px] rounded-lg border border-gray-600 bg-black p-2 text-white"
-    >
-      <button
-        @click="userStore.isLogoutOverlay = false"
-        class="flex w-full items-center justify-between bg-black p-3"
-      >
-        <div>Close Modal</div>
-        <Icon name="mdi:close" size="25" />
-      </button>
-      <div class="my-1 border-b border-b-gray-700" />
-      <button
-        @click="logout()"
-        class="flex w-full items-center justify-between bg-black p-3"
-      >
-        <div>Log Out</div>
-        <Icon name="ph:sign-out" size="25" />
-      </button>
+    <div id="Modal" class="fixed grid place-items-center h-screen z-50 bottom-0 bg-black bg-opacity-50 backdrop-blur-md h-screen w-full">
+        <div class="bg-black w-full max-w-[300px] rounded-lg text-white border border-gray-600 p-2">
+            <button 
+                @click="userStore.isLogoutOverlay = false" 
+                class="flex items-center justify-between bg-black w-full p-3"
+            >
+                <div>Close Modal</div>
+                <Icon name="mdi:close" size="25"/>
+            </button>
+            <div class="border-b border-b-gray-700 my-1" />
+            <button @click="logout()" class="flex items-center justify-between bg-black w-full p-3">
+                <div>Log Out</div>
+                <Icon name="ph:sign-out" size="25"/>
+            </button>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
-import { ROUTES } from '@/routes'
-import { useUserStore } from '@/stores/user'
+import { useUserStore } from '~/stores/user';
 const userStore = useUserStore()
-const { auth } = useSupabaseClient()
+const client = useSupabaseClient()
 
 const logout = () => {
-  auth.signOut()
-  userStore.isLogoutOverlay = false
-  navigateTo(ROUTES.login)
+    client.auth.signOut()
+    userStore.isLogoutOverlay = false
+    return navigateTo('/')
 }
 </script>
