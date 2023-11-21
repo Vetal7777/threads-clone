88880e2bf6d515c1d799637c688bfdaa1698660a
vetal7777 <jobschildren@gmail.com>
2023-11-21 23:24:36 +0200

    feat: update index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,84 +1,75 @@
 <template>
-  <MainLayout>
-    <div id="IndexPage" class="w-full overflow-auto">
-      <div class="mx-auto max-w-[500px] overflow-hidden">
-        <div id="Posts" class="mx-auto max-w-[600px] px-4">
-          <div v-if="isPosts" v-for="post in posts" :key="post">
-            <Post :post="post" @isDeleted="userStore.getAllPosts()" />
-          </div>
-          <div v-else>
-            <client-only>
-              <div
-                v-if="isLoading"
-                class="mx-auto mt-20 flex w-full items-center justify-center"
-              >
-                <div
-                  class="mx-auto flex flex-col items-center justify-center text-white"
-                >
-                  <Icon
-                    name="eos-icons:bubble-loading"
-                    size="50"
-                    color="#ffffff"
-                  />
-                  <div class="mt-1 w-full">Loading...</div>
+    <MainLayout>
+        <div id="IndexPage" class="w-full overflow-auto">
+            <div class="mx-auto max-w-[500px] overflow-hidden">
+                <div id="Posts" class="px-4 max-w-[600px] mx-auto" >
+
+                    <div v-if="isPosts" v-for="post in posts" :key="post">
+                        <Post :post="post" @isDeleted="posts = userStore.getAllPosts()" />
+                    </div>
+                    <div v-else>
+                        <client-only>
+                            <div v-if="isLoading" class="mt-20 w-full flex items-center justify-center mx-auto">
+                                <div class="text-white mx-auto flex flex-col items-center justify-center">
+                                    <Icon name="eos-icons:bubble-loading" size="50" color="#ffffff" />
+                                    <div class="w-full mt-1">Loading...</div>
+                                </div>
+                            </div>
+                            <div v-if="!isLoading" class="mt-20 w-full flex items-center justify-center mx-auto">
+                                <div class="text-white mx-auto flex flex-col items-center justify-center">
+                                    <Icon name="tabler:mood-empty" size="50" color="#ffffff" />
+                                    <div class="w-full">Make the first post!</div>
+                                </div>
+                            </div>
+                        </client-only>
+                    </div>
+                    <div class="mt-60" />
                 </div>
-              </div>
-              <div
-                v-if="!isLoading"
-                class="mx-auto mt-20 flex w-full items-center justify-center"
-              >
-                <div
-                  class="mx-auto flex flex-col items-center justify-center text-white"
-                >
-                  <Icon name="tabler:mood-empty" size="50" color="#ffffff" />
-                  <div class="w-full">Make the first post!</div>
-                </div>
-              </div>
-            </client-only>
-          </div>
-          <div class="mt-60" />
+            </div>
         </div>
-      </div>
-    </div>
-  </MainLayout>
+    </MainLayout>
 </template>
 
-<script setup lang="ts">
-import MainLayout from '@/layouts/MainLayout.vue'
-import { storeToRefs } from 'pinia'
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
 
-import { ROUTES } from '@/routes'
-import { useUserStore } from '@/stores/user'
+import { useUserStore } from '~/stores/user';
 const userStore = useUserStore()
-const { posts } = storeToRefs(userStore)
 const user = useSupabaseUser()
 
+let posts = ref([])
 let isPosts = ref(false)
 let isLoading = ref(false)
 
-watchEffect(async () => {
-  if (!user.value) {
-    return navigateTo(ROUTES.login)
-  } else {
-    await userStore.getAllPosts()
-  }
+watchEffect(() => {
+    if (!user.value) {
+        return navigateTo('/auth')
+    }
+})
+
+onBeforeMount(async () => {
+    try {
+        isLoading.value = true
+        await userStore.getAllPosts()
+        isLoading.value = false
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 onMounted(() => {
-  watchEffect(() => {
-    if (posts.value && posts.value.length) {
-      isPosts.value = true
-    }
-  })
+    watchEffect(() => {
+        if (userStore.posts && userStore.posts.length >= 1) {
+            posts.value = userStore.posts
+            isPosts.value = true
+        }
+    })
 })
 
-watch(
-  () => posts.value,
-  () => {
-    if (posts.value && posts.value.length) {
-      isPosts.value = true
+watch(() => posts.value, () => {
+    if (userStore.posts && userStore.posts.length >= 1) {
+        posts.value = userStore.posts
+        isPosts.value = true
     }
-  },
-  { deep: true }
-)
+}, { deep: true })
 </script>
