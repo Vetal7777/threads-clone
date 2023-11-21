2bf918381fa124b5a204ec4d31c52acf8400261d
vetal7777 <jobschildren@gmail.com>
2023-11-21 23:25:54 +0200

    chore: update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,47 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   pages: true,
   modules: [
-    'nuxt-icon',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/supabase',
-    '@pinia/nuxt',
-    '@vite-pwa/nuxt'
+      'nuxt-icon',
+      '@nuxtjs/tailwindcss',
+      '@nuxtjs/supabase',
+      '@pinia/nuxt',
+      "@vite-pwa/nuxt",
   ],
   runtimeConfig: {
     public: {
-      baseUrl: process.env.BASE_URL || 'http://localhost:3000/',
       bucketUrl: process.env.BUCKET_URL
     }
   },
   devtools: { enabled: false },
   pwa: {
     manifest: {
-      name: 'Threads Clone',
-      short_name: 'Threads Clone',
-      description: 'This is a Threads Clone',
-      theme_color: '#000000',
+      name: "Threads Clone",
+      short_name: "Threads Clone",
+      description: "This is a Threads Clone",
+      theme_color: "#000000",
       icons: [
         {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
+          src: "pwa-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
         },
         {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
+          src: "pwa-512x512.png",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
     },
     devOptions: {
       enabled: true,
-      type: 'module'
-    }
+      type: "module",
+    },
   },
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1, maximum-scale=1'
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
     }
   }
 })
