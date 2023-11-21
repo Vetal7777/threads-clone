21dddbe1c83b634abdc257cfd167efb7a484c27e
vetal7777 <jobschildren@gmail.com>
2023-11-21 23:23:28 +0200

    feat: update app.vue

--- app.vue
+++ app.vue
@@ -1,38 +1,26 @@
 <template>
   <VitePwaManifest />
 
-  <div class="fixed flex h-full w-[calc(100%+1px)] bg-black">
+  <div class="bg-black fixed w-[calc(100%+1px)] h-full">
     <NuxtPage />
 
     <CreatePost
       :class="[
-        {
-          'visible max-h-[100vh] transition-all duration-200 ease-in':
-            userStore.isMenuOverlay
-        },
-        {
-          'invisible max-h-0 transition-all duration-200 ease-out':
-            !userStore.isMenuOverlay
-        }
+          {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay },
+          {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay },
       ]"
     />
 
-    <Modal
+    <Modal 
       :class="[
-        {
-          'visible max-h-[100vh] transition-all duration-200 ease-in':
-            userStore.isLogoutOverlay
-        },
-        {
-          'invisible max-h-0 transition-all duration-200 ease-out':
-            !userStore.isLogoutOverlay
-        }
+          {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isLogoutOverlay },
+          {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isLogoutOverlay },
       ]"
     />
   </div>
 </template>
 
-<script setup lang="ts">
-import { useUserStore } from '@/stores/user'
+<script setup>
+import { useUserStore } from '~/stores/user';
 const userStore = useUserStore()
 </script>
